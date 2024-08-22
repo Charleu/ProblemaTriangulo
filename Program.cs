cec2a41e8885672a1f156f9ead7fdc4e6d0f8204
Charleu <sdlimeira0@gmail.com>
2024-08-22 18:21:57 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProblemaTriangulo
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            Console.WriteLine("Comparação entre triângulos\n");
+            Console.WriteLine("Informe as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine($"Área do triângulo X: {areaX:F2}");
+            Console.WriteLine($"Área do triângulo Y: {areaY:F2}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Triângulo X é maior");
+            }
+            else
+            {
+                Console.WriteLine("Triângulo Y é maior");
+            }
+        }
+    }
+}
